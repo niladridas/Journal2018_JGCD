b40f7f0868c84d3b38cc992273ba4fbedd1e5d0c
NILADRI DAS <niladridas95@gmail.com>
2018-12-04 15:33:01 -0600

    plot sigma bounds

--- Paper_Codes/main_folder/Source_files/inovestimate.m
+++ Paper_Codes/main_folder/Source_files/inovestimate.m
@@ -1,10 +1,6 @@
-function W_tmp = inovestimate(x_state,z, PTRh,MJD_UTCtime,MJD_J2000,eopdata,Arcs,lambda,  station_ecefxyz, st_latlongalt, GM_Earth,R_Earth ,f_Earth)
-    M = size(x_state,2); % number of samples
+function W_tmp = inovestimate(eci_states,z, PTRh,MJD_UTCtime,MJD_J2000,eopdata,Arcs,lambda,  station_ecefxyz, st_latlongalt,R_Earth ,f_Earth)
+    M = size(eci_states,2); % number of samples
     W_tmp = zeros(1,M); % Sample innovation 
-    eci_states = zeros(6,M);
-    for j = 1:M
-        eci_states(:,j) = coe2eci(mee2coe(x_state(:,j)),GM_Earth)';
-    end
     station_ecixyz = recef2eci(MJD_UTCtime, station_ecefxyz,MJD_J2000,eopdata,Arcs);
     sat_rvec = eci_states(1:3,:)-repmat(station_ecixyz,1,M);
     sat_rnorm = sqrt(sum(sat_rvec.^2, 1));
@@ -16,6 +12,6 @@ function W_tmp = inovestimate(x_state,z, PTRh,MJD_UTCtime,MJD_J2000,eopdata,Arcs
         r_delr(1,i) = sat_rnorm(1,i) + r_delay_OT;
     end
     for i = 1:M 
-        W_tmp(1,i) = (z(2,1)-r_delr(1,i)); 
+        W_tmp(1,i) = (z-r_delr(1,i)); 
     end
 end
